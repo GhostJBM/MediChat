bb72019e4b5b2671dd48d0b8aa30b80170178585
GhostJBM <Josielbenavidez@gmail.com>
2025-09-26 17:42:13 -0600

    API maps

---
+++ Backend/modulos/src/routes/mapsRoutes.js
@@ -0,0 +1,19 @@
+    import express from 'express'
+    import { BuscarHospitales } from '../services/maps.js'
+
+const routerMap = express.Router()
+
+//End point de la api para buscar hospitales cercanos ruta de acceso /MapHospitales
+routerMap.get('/MapHospitales', async (req, res) => {
+    const {lat, lng, radius} = req.body;
+
+        try{
+            const places = await BuscarHospitales(Number(lat), Number(lng), radius ? Number(radius) : 5000);
+            res.json(places)
+        }
+        catch(err){
+            res.status(500).json({error : "Error buscando los hospitales"})
+        }
+})
+
+export default routerMap;
---
+++ Backend/modulos/src/services/maps.js
@@ -0,0 +1,35 @@
+import { Client } from "@googlemaps/google-maps-services-js";
+import dotenv from "dotenv"
+
+//configuracion  de la apinp
+dotenv.config();
+const client = new Client({})
+
+export async function BuscarHospitales(lat, lng, radius = 5000) {
+    try{
+        const apiKey = "AIzaSyDWUXFYwmmgDvmjejfS5y8OJUsye5dYRzc"
+
+        const Url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=5000&keyword=hospital&key=${apiKey}`;
+
+
+        const response = await fetch( Url)
+        const data = await response.json();
+
+        if(data.status === "OK"){
+            
+
+            return data.results.map(place =>({
+            name: place.name,
+            address: place.vicinity,
+            location: place.geometry.location,
+            place_id: place.place_id
+            }))
+        }else {
+            throw new Error("Error en el lugar de origen: " + data.status);
+        }
+    }
+    catch(err){
+        console.error(err.message);
+        throw err;
+    }
+}
